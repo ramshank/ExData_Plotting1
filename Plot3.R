ca057c84753e229ec2221063956e2f709f596369
ramshank <ramshank@hotmail.com>
2014-07-12 11:49:02 +0200

    Updating Plot 3 to address rendering issue for 480 x 480

--- Plot3.R
+++ Plot3.R
@@ -11,15 +11,16 @@ if(!exists("epcdata")) {
 ## filter relevant data
 epcfdata<-epcdata[(epcdata$Date>=as.Date("2007-02-01")& epcdata$Date<=as.Date("2007-02-02")),]
 
+png(filename="Plot3.png", width=480,height=480)
 #plot the graph
 with(epcfdata, plot(epcfdata$Time,Sub_metering_1,type="l",xlab="",ylab="Energy sub metering"))
 with(epcfdata, points(epcfdata$Time,Sub_metering_2,type="l", col="red"))
 with(epcfdata, points(epcfdata$Time,Sub_metering_3,type="l",col="blue"))
 
 #add the legend
-legend("topright","(x,y)", lty=1, col = c("black","red","blue"), legend = c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"))
+legend("topright","(x,y)",cex=0.8, lty=1, col = c("black","red","blue"), legend = c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"))
 
 #save as png file
-dev.copy(png, file="Plot3.png",height=480, width=640 )
+#dev.copy(png, file="Plot3.png",height=480, width=480 )
 
 dev.off()
